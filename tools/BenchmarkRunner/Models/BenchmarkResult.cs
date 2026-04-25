1aab4f959bc56938910c71a14ac034ffea5ed6cf
sergey.bulavskiy <sergey.bulavskiy@gmail.com>
2026-04-25 18:32:45 +0700

    feat: добавить MigrationSeconds и ResetSeconds в BenchmarkResult

--- tools/BenchmarkRunner/Models/BenchmarkResult.cs
+++ tools/BenchmarkRunner/Models/BenchmarkResult.cs
@@ -6,9 +6,13 @@ namespace BenchmarkRunner.Models;
 /// </summary>
 /// <param name="Scenario">Сценарий бенчмарка</param>
 /// <param name="ElapsedSeconds">Время выполнения в секундах</param>
+/// <param name="MigrationSeconds">Суммарное время миграций в секундах</param>
+/// <param name="ResetSeconds">Суммарное время сброса данных в секундах</param>
 /// <param name="Success">Успешное ли выполнение</param>
 public record BenchmarkResult(
     BenchmarkScenario Scenario,
     double ElapsedSeconds,
+    double MigrationSeconds,
+    double ResetSeconds,
     bool Success
 );
